807235737346fb130d29bc396e98d45ae438b36f
ARABIA <ARABIA@DESKTOP-C037G9B>
2024-06-02 16:11:04 +0300

    Last Commit

---
+++ MoviesECommerce/MoviesE-commerce/Migrations/20240601124657_mm.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoviesE_commerce.Migrations
+{
+    public partial class mm : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
